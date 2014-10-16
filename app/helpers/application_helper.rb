0070a9ffc03c6d31e7333c012b85bf95909cfbd2
Stephen Strudwick <stephenstrudwick@Stephens-MacBook-Pro.local>
2014-10-16 16:41:07 +0100

    added primary button helper for bootstrap view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def primary_button(text, path, html_options)
+    options = merge_default_html_options({ :class => [ 'btn', 'btn-primary'] }, html_options)
+
+    link_to text, path, html_options
+  end
+
+  private
+
+  def merge_default_html_options(defaults, html_options)
+    defaults.keys.each do |default_key|
+      html_options.keys.each do |key|
+        if key == default_key
+          values = html_options[key].split(/s+/)
+          defaults[key].each{|value| values.push(value) }
+
+          html_options[key] = values.join(' ')
+        end
+      end
+    end
+
+    html_options
+  end
+
 end
